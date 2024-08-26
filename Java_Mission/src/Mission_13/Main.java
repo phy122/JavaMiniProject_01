e9ec1555d5d8b18b42e9ec8670390095856874ac
phy122 <qkrghdud369@gmail.com>
2024-08-26 18:22:10 +0900

    수정

--- Java_Mission/src/Mission_13/Main.java
+++ Java_Mission/src/Mission_13/Main.java
@@ -1,24 +1,27 @@
 package Mission_13;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 		LottoFunction function = LottoFunction.getInstance();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd(E) HH:mm:ss");
 		Ticket ticket = new Ticket();
 		Scanner scanner = new Scanner(System.in);
 		 
 		System.out.print("몇 게임? ");
         int numberOfGames = scanner.nextInt();
         scanner.nextLine(); 
-        
+       
         for (int i = 0; i < numberOfGames; i++) {
             System.out.print("[" + (i + 1) + " 게임] (1.자동 / 2.수동) : ");
             int choice = scanner.nextInt();
             scanner.nextLine();
 
             if (choice == 1) { 
+            	
                 ticket.addGame(new Game(function.RandomNumbers()));
             } else if (choice == 2) {
             	
@@ -31,10 +34,12 @@ public class Main {
             }
         }
      // 로또 티켓 출력
+        
         System.out.println("############ 인생역전 Lottoria ############");
-        System.out.println("발행일 : " + function.getLottoDate());
-        System.out.println("추첨일 : " + function.getDrawingDate());
-        System.out.println("지급기한 : " + function.getPaymentDate());
+        System.out.println("발행일 : " + sdf.format(function.getLottoDate()));
+        System.out.println("추첨일 : " + sdf.format(function.getDrawingDate()));
+        System.out.println("지급기한 : " + sdf.format(function.getPaymentDate()));
+        System.out.println("-----------------------------------------");
         ticket.categoryTicket();
 
         // 당첨 결과 확인
